6d9d34b0e818aba4949e3cfb7e292797240fc608
Joshua Blömer <55506217+joshuabloemer@users.noreply.github.com>
2023-03-25 14:00:49 +0100

    Improve Generators
    * Fix hash
    
    * Port python generator
    
    * Fix car id missing
    
    * Fix car ids
    
    * Process exceptions correctly
    
    * Adjust test values
    
    ---------
    
    Co-authored-by: isiko <isihd.ko@gmail.com>

--- generator/generateInput.js
+++ generator/generateInput.js
@@ -2,7 +2,12 @@
 // The Parameters are exactly the output of the corresponding generators.
 
 function generateInput(files) {
-    return "quit";
+    let output = "load .\nsimulate 10000\n";
+    for (let car of files[2].raw) {
+        output += `position ${car}\n`;
+    }
+    output += "quit";
+    return output;
 }
 
 module.exports = generateInput;
