bbebcaf40c92a8128c3bd7ddae645951b67bcd09
Joshua Blömer <55506217+joshuabloemer@users.noreply.github.com>
2023-03-25 14:00:49 +0100

    Improve Generators
    * Fix hash
    
    * Port python generator
    
    * Fix car id missing
    
    * Fix car ids
    
    * Process exceptions correctly
    
    * Adjust test values
    
    ---------
    
    Co-authored-by: isiko <isihd.ko@gmail.com>

--- generator/generateFiles.js
+++ generator/generateFiles.js
@@ -7,132 +7,150 @@
 // Only the parameters show above are used by the rest of the Code, so you can add additional parameters if you want.
 // The function should not write any files to the disk.
 
-function generateFiles() {
-    // Some Helper Functions
-    function randomBoolean(){
-        return Math.random() < 0.5;
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        let temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
     }
+}
 
-    function randomInt(min, max){
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    let crossingAmount = randomInt(2, 10);
-    let streetAmount = randomInt(crossingAmount, crossingAmount * 4);
 
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+function generate_full_network(crossing) {
     let crossings = [];
-    for (let i = 0; i < crossingAmount; i++) {
-        crossings.push({
-            id: i,
-            timer: randomBoolean() ? randomInt(3, 10) : 0,
-        });
-    }
-
     let streets = [];
-    let maxCarAmount = 0;
-    for (let i = 0; i < streetAmount; i++) {
-        let street = {
-            start: randomInt(0, crossingAmount - 1),
-            end: randomInt(0, crossingAmount - 1),
-            length: randomInt(10, 10000),
-            speedLimit: randomInt(5, 40),
-            allowOvertaking: randomInt(1, 2),
-            carAmount: 0,
-        }
 
-        let invalid = false;
-        for (let crossing of crossings) {
-            let incoming = 0;
-            let outgoing = 0;
-            for (let otherStreet of streets) {
-                if (otherStreet.start == crossing.id) {
-                    outgoing++;
-                }
-                if (otherStreet.end == crossing.id) {
-                    incoming++;
+    for (let i = 0; i < crossing; i++) {
+        crossings.push([4, 4]);
+        for (let j = 0; j < 4; j++) {
+            let end = Math.floor(Math.random() * crossing);
+
+            sum = 0;
+            for (let [i, street] of streets.entries()) {
+                if (end == street[1]) {
+                    sum += 1;
                 }
             }
-
-            // Make sure that every crossing has at least one incoming and one outgoing street
-            if (incoming == 0 && street.end !== crossing.id && street.start !== crossing.id) {
-                street.end = crossing.id;
-                incoming++;
+            let retry = 0;
+            while ((end == i || sum >= 4) && retry < crossing * 4) {
+                end = Math.floor(Math.random() * crossing);
+                sum = 0;
+                for (let [i, street] of streets.entries()) {
+                    if (end == street[1]) {
+                        sum += 1;
+                    }
+                }
+                retry++;
+            }
+            if (retry >= crossing * 4) {
+                continue;
             }
 
-            if (outgoing == 0 && street.start !== crossing.id && street.end !== crossing.id) {
-                street.start = crossing.id;
-                outgoing++;
+            let street = [i, end];
+            streets.push(street);
+        }
+    }
+    return [crossings, streets];
+}
+
+function mutate_network(network, street_density) {
+    let randomized = [...network[1]];
+    shuffleArray(randomized);
+    let streets_copy = [...randomized];
+    let removed_streets = 0;
+    let copy = [...streets_copy];
+
+    for (let [i, street] of randomized.entries()) {
+        if (removed_streets >= randomized.length * (1 - street_density)) {
+            break;
+        }
+        let crossings = network[0];
+        let start_outgoing = crossings[street[0]][1] - 1;
+        let end_incoming = crossings[street[1]][0] - 1;
+
+        if (start_outgoing >= 1 && start_outgoing <= 4 && end_incoming >= 1 && end_incoming <= 4) {
+            const index = copy.indexOf(5);
+            if (index > -1) { // only splice array when item is found
+                copy.splice(index, 1); // 2nd parameter means remove one item only
             }
+            crossings[street[0]][1] -= 1;
+            crossings[street[1]][0] -= 1;
+            removed_streets += 1;
+        }
+    }
+    return [network[0], copy];
+}
 
-            if (incoming > 4 && street.end == crossing.id) {
-                invalid = true;
-                break;
+function generate_street_attributes(network, double_lane_weight) {
+    let new_streets = [];
+    for (let [i, street] of network[1].entries()) {
+        let length = randomIntFromInterval(40, 200);
+        // length = Math.ceil(n / (Math.log(n) * Math.log(n)));
+        let lanes = 1;
+        if (Math.random() < double_lane_weight) {
+            lanes = 2;
+        }
+        let max_speed = randomIntFromInterval(5, 40);
+        new_streets.push([street[0], street[1], length, lanes, max_speed]);
+    }
+    return [network[0], new_streets];
+}
+
+
+function generateFiles() {
+    let number_of_crossings = randomIntFromInterval(20, 100);
+    let street_density = Math.random();
+    let double_lane_weight = Math.random();
+    let car_density = Math.random();
+    let round_about_density = Math.random();
+
+    let streets = '';
+    let crossings = '';
+    let carsString = '';
+    let cars = [];
+    let network = generate_full_network(number_of_crossings);
+    network = mutate_network(network, street_density);
+    network = generate_street_attributes(network, double_lane_weight);
+
+    for (let [i, street] of network[1].entries()) {
+        streets += `${street[0]}-->${street[1]}:${street[2]}m,${street[3]}x,${street[4]}max\n`;
+    }
+
+    for (let i = 0; i < network[0].length; i++) {
+        crossings += `${i}:${Math.random() < round_about_density ? 0 : randomIntFromInterval(3, 10)}t\n`;
+    }
+
+    let car_id = 0
+    for (let [i, street] of network[1].entries()) {
+        for (let j = 0; j < street[2]; j += 10) {
+            if (Math.random() < car_density) {
+                carsString += `${car_id},${i},${randomIntFromInterval(20, 40)},${randomIntFromInterval(1, 10)}\n`;
+                cars.push(car_id);
+                car_id++;
+
             }
-            if (outgoing > 4 && street.start == crossing.id) {
-                invalid = true;
-                break;
-            }
-        }
-        if (invalid || street.start == street.end) {
-            i--;
-            continue;
-        }
-        
-
-        maxCarAmount += Math.floor(street.length / 10);
-
-        streets.push(street);
-    }
-
-    const absoulteMaxCarAmount = 5000;
-    maxCarAmount = Math.min(maxCarAmount, absoulteMaxCarAmount);
-    let carAmount = randomInt(1, maxCarAmount);
-    cars = [];
-
-    for (let i = 0; i < carAmount; i++) {
-        let car = {
-            id: i,
-            street: randomInt(0, streetAmount - 1),
-            speed: randomInt(20, 40),
-            acceleration: randomInt(1, 10),
-        }
-
-        if (streets[car.street].length > streets[car.street].carAmount * 10) {
-            streets[car.street].carAmount++;
-            cars.push(car);
-        } else {
-            i--;
         }
     }
-
-    let crossingString = "";
-    for (let crossing of crossings) {
-        crossingString += `${crossing.id}:${crossing.timer}t\n`;
-    }
-
-    let streetString = "";
-    for (let street of streets) {
-        streetString += `${street.start}-->${street.end}:${street.length}m,${street.allowOvertaking}x,${street.speedLimit}max\n`;
-    }
-
-    let carString = "";
-    for (let car of cars) {
-        carString += `${car.id},${car.street},${car.speed},${car.acceleration}\n`;
-    }
-
     return [
         {
             name: "crossings.sim",
-            content: crossingString,
+            content: crossings,
             raw: crossings,
         },
         {
             name: "streets.sim",
-            content: streetString,
+            content: streets,
             raw: streets,
         },
         {
             name: "cars.sim",
-            content: carString,
+            content: carsString,
             raw: cars,
         }
     ];
--- index.js
+++ index.js
@@ -20,7 +20,7 @@ console.log(`JAR path: ${jarPath}`);
 console.log("Loading Generators");
 const generateFiles = require('./generator/generateFiles');
 const generateInput = require('./generator/generateInput');
-const validateInput = require('./generator/validateInput');
+// const validateInput = require('./generator/validateInput');
 
 // Load JARs
 console.log(`Loading JARs from ${path.resolve(jarPath)}`);
@@ -36,14 +36,17 @@ if (jarFiles.length == 0) {
 function runJAR(jar, currentOutputDir, input) {
     console.log(`Running JAR ${jar}`);
     const date = Date.now();
-    const output = child_process.execSync(`java -jar ${path.resolve(jarPath, jar)}`, { 
-        timeout: 1000 * 60 * 5,    // 5 Minutes in ms
-        cwd: currentOutputDir,     // Working directory
-        input: input,              // Input
-        stdio: [null, null, null]  // Don't print anything in the console
-    });
+    let output;
+    try {
+        output = child_process.execSync(`java -jar ${path.resolve(jarPath, jar)}`, {
+            timeout: 1000 * 60 * 5,    // 5 Minutes in ms
+            cwd: currentOutputDir,     // Working directory
+            input: input,              // Input
+            stdio: [null, null, null]  // Don't print anything in the console
+        });
+    } catch (err) { output = err.stderr.toString() }
     const outputHash = crypto.createHash('sha256').update(output).digest('hex');
-    let outputFile = path.resolve(currentOutputDir, outputHash + "." +  jar.replace(".jar", ".out"));
+    let outputFile = path.resolve(currentOutputDir, outputHash + "." + jar.replace(".jar", ".out"));
     console.log(`Finished JAR ${jar} in ${Date.now() - date}ms -> ${outputHash}`);
     fs.writeFileSync(outputFile, output);
 }
@@ -55,6 +58,7 @@ if (reRun) {
     while (completedFiles.length < fs.readdirSync(outputDir).length) {
         console.log();
         let files = fs.readdirSync(outputDir);
+        let file = files[Math.floor(Math.random() * files.length - 1)];
         let file = files[Math.floor(Math.random() * files.length-1)];
         if (completedFiles.includes(file)) {
             continue;
@@ -96,14 +100,14 @@ while (true) {
     console.log("Generating Input");
     const inputFiles = generateFiles();
     const input = generateInput(inputFiles);
-    const hash = crypto.createHash('sha256').update(input).update(inputFiles.toString()).digest('hex');
+    const hash = crypto.createHash('sha256').update(inputFiles[0].content).update(inputFiles[1].content).update(inputFiles[2].content).digest('hex');
     console.log(`Generated Input (Hash: ${hash})`);
 
-    // Validate Input
-    if (!validateInput(input, inputFiles)) {
-        console.log("Skipping Input (Invalid)");
-        continue;
-    }
+    // // Validate Input
+    // if (!validateInput(input, inputFiles)) {
+    //     console.log("Skipping Input (Invalid)");
+    //     continue;
+    // }
 
     const currentOutputDir = path.resolve(outputDir, hash);
     if (fs.existsSync(currentOutputDir)) {
